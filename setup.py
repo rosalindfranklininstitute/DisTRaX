6141b7c5f23f1647285f5c1eb03c0a005e189935
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2023-01-16 12:48:53 +0000

    Config/setup (#3)
    * Updated .pre-commit-config.yaml
    
    Removed double-quote-string-fixer as it clashes with black
    
    * Adding distrax src folder
    
    * Added setup.py
    
    This allows the package to be built and installed.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    version="0.0.1",
+    name="distrax",
+    description="a fast and scalable deployment tool to create quick and efficient \
+                 storage systems onto high-performance computing (HPC) \
+                 compute nodes utilising the compute nodes system memory/storage.",
+    url="https://github.com/rosalindfranklininstitute/distrax",
+    author="Gabryel Mason-Williams",
+    author_email="gabryel.mason-williams@rfi.ac.uk",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    test_suite="tests",
+    classifiers=[
+        "License :: OSI Approved :: Apache License, Version 2.0",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: POSIX :: Linux",
+    ],
+    license="Apache License, Version 2.0",
+    zip_safe=False,
+    install_requires=[],
+)
