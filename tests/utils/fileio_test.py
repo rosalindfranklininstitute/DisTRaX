6b77515b01cf3c9ac947289872fde6a26d0fd899
= <gabryelmw@gmail.com>
2023-02-16 15:01:54 +0000

    Tests for fileIO
    Tests for file and folder control. 2 out of the 6 tests will fail if the user is not root or the test_python_user USER and test_python_group and GRP are not present.

---
+++ tests/utils/fileio_test.py
@@ -0,0 +1,150 @@
+from pathlib import Path
+import distrax.utils.fileio as fileio
+import pytest
+import os
+import stat
+import shutil
+import pwd
+import grp
+
+TEST_FOLDER = "TEST_FOLDER"
+TEST_SUB_FOLDER = TEST_FOLDER + "/" + TEST_FOLDER
+TEST_FILE = "TEST_FILE.txt"
+NON_EXISTENT_FILE = "NON_EXISTENT_FILE.txt"
+TEST_USER = "test_python_user"
+TEST_GRP = "test_python_group"
+
+
+@pytest.fixture()
+def folder_resource():
+    """
+    Creates a folder and removes after testing
+    """
+    if os.path.exists(TEST_FOLDER):
+        shutil.rmtree(TEST_FOLDER)
+    os.mkdir(TEST_FOLDER)
+    yield "text_file_resource"
+    if os.path.exists(TEST_FOLDER):
+        shutil.rmtree(TEST_FOLDER)
+
+
+@pytest.fixture()
+def sub_folder_file_resource():
+    """
+    Creates a folder and removes after testing
+    """
+    if os.path.exists(TEST_FOLDER):
+        shutil.rmtree(TEST_FOLDER)
+    os.mkdir(TEST_FOLDER)
+    os.mkdir(TEST_SUB_FOLDER)
+    open(TEST_SUB_FOLDER + "/" + TEST_FILE, "w").close()
+    yield "text_file_resource"
+    if os.path.exists(TEST_FOLDER):
+        shutil.rmtree(TEST_FOLDER)
+
+
+@pytest.fixture()
+def file_resource():
+    """
+    Creates a textfile and removes after testing
+    """
+    if os.path.exists(TEST_FILE):
+        os.remove(TEST_FILE)
+    open(TEST_FILE, "w").close()
+    yield "text_file_resource"
+    if os.path.exists(TEST_FILE):
+        os.remove(TEST_FILE)
+
+
+class TestFileIO:
+    """
+    Tests functions within FileIO
+    """
+
+    def test_create_create_dir(self):
+        mode = 777
+        test = fileio.create_dir(TEST_FOLDER, mode)
+        assert test is True
+        assert stat.S_IMODE(os.lstat(TEST_FOLDER).st_mode) == mode
+        # Create folder that already exists
+        test = fileio.create_dir(TEST_FOLDER, mode)
+        assert test is False
+        os.removedirs(TEST_FOLDER)
+
+    def test_remove_dir(self, folder_resource):
+        test = fileio.remove_dir(TEST_FOLDER)
+        assert test is True
+        test = fileio.remove_dir(TEST_FOLDER)
+        assert test is False
+
+    def test_copy_file(self, folder_resource, file_resource):
+        test = fileio.copy_file(TEST_FILE, TEST_FOLDER)
+        assert test is True
+        assert os.path.exists(TEST_FILE)
+        assert os.path.exists(TEST_FOLDER + "/" + TEST_FILE)
+        test = fileio.copy_file(NON_EXISTENT_FILE, TEST_FOLDER)
+        assert test is False
+
+    def test_remove_file(self, file_resource):
+        test = fileio.remove_file(TEST_FILE)
+        assert test is True
+        test = fileio.remove_file(TEST_FILE)
+        assert test is False
+
+    def test_change_ownership(self, file_resource):
+        if os.geteuid() != 0:
+            pytest.fail("Need to run as ROOT to pass this test")
+        try:
+            pwd.getpwnam(TEST_USER)
+            grp.getgrnam(TEST_GRP)
+            current_user = Path(TEST_FILE).owner()
+            current_grp = Path(TEST_FILE).group()
+            assert current_user != TEST_USER
+            assert current_grp != TEST_GRP
+            test = fileio.change_ownership(TEST_FILE, TEST_USER, TEST_GRP)
+            assert test is True
+            assert Path(TEST_FILE).owner() == TEST_USER
+            assert Path(TEST_FILE).group() == TEST_GRP
+            test = fileio.change_ownership(NON_EXISTENT_FILE, TEST_USER, TEST_GRP)
+            assert test is False
+        except KeyError:
+            pytest.fail(
+                "\nExpecting TEST_USER (test_python_user) and "
+                + "TEST_GRP (test_python_group) "
+                + "This test will auto fail if they are not present. "
+                + "This requires being run in root.\n",
+                "",
+            )
+
+    def test_recursive_change_ownership(self, sub_folder_file_resource):
+        if os.geteuid() != 0:
+            pytest.fail("Need to run as ROOT to pass this test")
+        try:
+            pwd.getpwnam(TEST_USER)
+            grp.getgrnam(TEST_GRP)
+            current_user = Path(TEST_FOLDER).owner()
+            current_grp = Path(TEST_FOLDER).group()
+            assert current_user != TEST_USER
+            assert current_grp != TEST_GRP
+            current_sub_user = Path(TEST_SUB_FOLDER).owner()
+            current_sub_grp = Path(TEST_SUB_FOLDER).group()
+            assert current_sub_user != TEST_USER
+            assert current_sub_grp != TEST_GRP
+            test = fileio.recursive_change_ownership(TEST_FOLDER, TEST_USER, TEST_GRP)
+            assert test is True
+            assert Path(TEST_FOLDER).owner() == TEST_USER
+            assert Path(TEST_FOLDER).group() == TEST_GRP
+            assert Path(TEST_SUB_FOLDER).owner() == TEST_USER
+            assert Path(TEST_SUB_FOLDER).group() == TEST_GRP
+            test = fileio.recursive_change_ownership(
+                NON_EXISTENT_FILE, TEST_USER, TEST_GRP
+            )
+            assert test is False
+        except KeyError:
+            pytest.fail(
+                "\nExpecting TEST_USER (test_python_user) and "
+                + "TEST_GRP (test_python_group) "
+                + "This test will auto fail if they are not present. "
+                + "This requires being run in root.\n",
+                "",
+            )
