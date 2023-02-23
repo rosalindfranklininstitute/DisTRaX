ff82045576a539f1d1dc4b4f4b0429bfd0e2c0da
= <gabryelmw@gmail.com>
2023-02-23 12:13:13 +0000

    Sphinx Config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,16 +5,33 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "DisTRaX"
 copyright = "2023, Gabryel Mason-Williams"
 author = "Gabryel Mason-Williams"
-release = "0.0.1"
+# release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+    "sphinx_remove_toctrees",
+    "sphinx_design",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -23,5 +40,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
