61c713eee778c70ac1745b46d87b4dba0e533930
= <gabryelmw@gmail.com>
2023-03-16 11:25:34 +0000

    Tests for network util
    Tests for ip accessing from the network interface and getting the netmask from the ip.

---
+++ tests/utils/network_test.py
@@ -0,0 +1,18 @@
+import distrax.utils.network as network
+import pytest
+
+
+class TestNetwork:
+    """
+    Tests functions within network
+    """
+
+    def test_ip_address_from_network_interface(self):
+        interface = "lo"
+        assert network.ip_address_from_network_interface(interface) == "127.0.0.1"
+        # Check that OSError is raised
+        with pytest.raises(OSError):
+            network.ip_address_from_network_interface("1234")
+
+    def test_ip_with_netmask(self):
+        assert network.ip_with_netmask("127.0.0.1") == "127.0.0.1/32"
