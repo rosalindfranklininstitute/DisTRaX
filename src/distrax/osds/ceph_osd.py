718734c419e1112b083d8ab686458e17efc573b7
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2023-08-10 13:53:32 +0100

    Docs/osds (#35)
    * Update Fileio
    
    * Correcting the init debug message for OSDS
    
    * Simplifying remove_osds
    
    Remove OSDs should remove and OSD on the system to ensure DisTRaX leaves a clean system.
    
    * Simplifying remove_osds
    
    Now removes the ceph system from any block device that has the virtual group ceph.
    
    * Logging device creation.

--- src/distrax/osds/ceph_osd.py
+++ src/distrax/osds/ceph_osd.py
@@ -1,11 +1,15 @@
 import glob
+import logging
 import subprocess
+import time
 from typing import List
 
 import distrax.utils.ceph as ceph
 import distrax.utils.fileio as fileio
 import distrax.utils.system as system
 from distrax.osds import OSD
+
+logger = logging.getLogger(__name__)
 
 
 class CephOSD:
@@ -38,40 +42,44 @@ class CephOSD:
             >>> osd.create_osds(["/dev/ram0", "/dev/ram1"])
         """
         # Create needed directories for OSDs to run on the system
-        fileio.create_dir(ceph.VAR_BOOTSTRAP_OSD, 0o755)
-        fileio.recursive_change_ownership(ceph.VAR_BOOTSTRAP_OSD, "ceph", "ceph")
-        fileio.create_dir(ceph.VAR_OSD, 0o755)
-        fileio.recursive_change_ownership(ceph.VAR_OSD, "ceph", "ceph")
+        fileio.create_dir(ceph.VAR_BOOTSTRAP_OSD, 755, admin=True)
+        fileio.create_dir(ceph.VAR_OSD, 755, admin=True)
 
         # Copy OSDs files to ETC Ceph
         fileio.copy_file(
-            f"{self.folder}/{ceph.CONFIG_FILE}", f"{ceph.ETC_CEPH}/{ceph.CONFIG_FILE}"
+            f"{self.folder}/{ceph.CONFIG_FILE}",
+            f"{ceph.ETC_CEPH}/{ceph.CONFIG_FILE}",
+            admin=True,
         )
         fileio.copy_file(
             f"{self.folder}/{ceph.ADMIN_KEYRING}",
             f"{ceph.ETC_CEPH}/{ceph.ADMIN_KEYRING}",
+            admin=True,
         )
 
         fileio.copy_file(
-            f"{self.folder}/{ceph.OSD_KEYRING}", f"{ceph.ETC_CEPH}/ceph.keyring"
+            f"{self.folder}/{ceph.OSD_KEYRING}",
+            f"{ceph.ETC_CEPH}/ceph.keyring",
+            admin=True,
         )
         # Copy OSDs files to VAR Ceph
         fileio.copy_file(
             f"{self.folder}/{ceph.OSD_KEYRING}",
             f"{ceph.VAR_BOOTSTRAP_OSD}/ceph.keyring",
+            admin=True,
         )
         # Create the OSDs using ceph-volume
         for device in devices:
-            subprocess.run(["ceph-volume", "lvm", "create", "--data", device])
+            subprocess.run(
+                ["sudo", "ceph-volume", "lvm", "create", "--data", device],
+            )
+            logger.info(f"Created {device} OSD")
 
-    def remove_osds(self, devices: List[str]) -> None:
+    def remove_osds(self) -> None:
         """Remove the OSDs devices from the system.
 
-        Args:
-            devices: The device names to turn into OSDs
-
         Examples:
-            >>> osd.remove_osds(["/dev/ram0", "/dev/ram1"])
+            >>> osd.remove_osds()
         """
         # Get OSD_ids from the host system
         osd_ids = glob.glob(f"{ceph.VAR_OSD_ID}*")
@@ -81,24 +89,42 @@ class CephOSD:
             subprocess.run(
                 ["ceph", "osd", "out", str(osd_id), "--connect-timeout", "5"]
             )
+            # Wait for the osd to be set to out
+            time.sleep(1)
+            # Stop services
             system.stop_service(f"ceph-osd@{osd_id}")
             system.stop_service(f"var-lib-ceph-osd-ceph\\x2d{osd_id}.mount")
         # Stop OSDs service
         system.stop_service("ceph-osd.target")
         system.disable_service("ceph-osd.target")
         # Remove ceph and clean devices
-        for device in devices:
-            subprocess.run(["ceph-volume", "lvm", "zap", "--destroy", device])
+        pvs_process = subprocess.run(
+            ["sudo", "pvs", "--separator", ",", "-o", "pv_name,vg_name"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        pvs_output = pvs_process.stdout.decode("utf-8")
+        pvs_output_lst = pvs_output.replace(" ", "").splitlines()
+        for pv in pvs_output_lst:
+            pvs = pv.split(",")  # PV, VG
+            if pvs[1][:4] == "ceph":
+                subprocess.run(
+                    ["sudo", "ceph-volume", "lvm", "zap", "--destroy", pv[0]],
+                )
         # Get ceph-volume services
         osd_services = glob.glob(
             "/etc/systemd/system/multi-user.target.wants/ceph-volume@lvm-*"
         )
         # remove ceph-volume services
         for osd_service in osd_services:
-            fileio.remove_file(osd_service)
+            fileio.remove_file(osd_service, admin=True)
+        for osd_id in osd_ids:
+            osd_id = osd_id.strip(ceph.VAR_OSD_ID)
+            fileio.remove_file(f"{ceph.VAR_RUN}osd.{osd_id}.asok", admin=True)
         system.stop_service("system-ceph\\x2dosd.slice")
-        fileio.remove_dir(ceph.VAR_BOOTSTRAP_OSD)
-        fileio.remove_dir(ceph.VAR_OSD)
+        system.stop_service("system-ceph\\x2dvolume.slice")
+        fileio.remove_dir(ceph.VAR_BOOTSTRAP_OSD, admin=True)
+        fileio.remove_dir(ceph.VAR_OSD, admin=True)
 
 
 _osd = OSD("ceph", CephOSD)
