10cc520d0d838700ccd241c15985837c48106501
= <gabryelmw@gmail.com>
2023-08-10 12:35:29 +0100

    Additional system-wide Exceptions

--- src/distrax/exceptions/exceptions.py
+++ src/distrax/exceptions/exceptions.py
@@ -20,3 +20,27 @@ class DeviceCreationError(Exception):
             message: Message to display
         """
         super().__init__(message)
+
+
+class ClusterExistsError(Exception):
+    """When a Cluster Already Exists."""
+
+    def __init__(self, message: str):
+        """Call the base class constructor with the parameters it needs.
+
+        Args:
+            message: Message to display
+        """
+        super().__init__(message)
+
+
+class DaemonNotStartedError(Exception):
+    """When a Daemon Fails to Start."""
+
+    def __init__(self, message: str):
+        """Call the base class constructor with the parameters it needs.
+
+        Args:
+            message: Message to display
+        """
+        super().__init__(message)
