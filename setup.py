7a707f5853ae8c0a11d9fdae2060251a453401ab
= <gabryelmw@gmail.com>
2023-08-10 18:35:17 +0100

    setup.py
    Recommend method: https://setuptools.pypa.io/en/latest/userguide/quickstart.html#setup-py

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    version="0.0.1",
-    name="distrax",
-    description="a fast and scalable deployment tool to create quick and efficient \
-                 storage systems onto high-performance computing (HPC) \
-                 compute nodes utilising the compute nodes system memory/storage.",
-    url="https://github.com/rosalindfranklininstitute/distrax",
-    author="Gabryel Mason-Williams",
-    author_email="gabryel.mason-williams@rfi.ac.uk",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    package_data={"distrax": ["py.typed"]},
-    test_suite="tests",
-    classifiers=[
-        "License :: OSI Approved :: Apache License, Version 2.0",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.10",
-        "Operating System :: POSIX :: Linux",
-    ],
-    license="Apache License, Version 2.0",
-    zip_safe=False,
-    install_requires=[],
-)
+setup()
