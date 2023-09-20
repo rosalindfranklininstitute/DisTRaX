2aec029fc3df8475a6b6ea3f00f7e6424c84b438
= <gabryelmw@gmail.com>
2023-09-20 09:21:33 +0100

    Updated function to include ip_address nd the netmask did not properly capture the correct information.
    This passes the updated tests.

--- src/distrax/utils/network.py
+++ src/distrax/utils/network.py
@@ -1,5 +1,4 @@
 import fcntl
-import ipaddress
 import logging
 import socket
 import struct
@@ -7,7 +6,9 @@ import struct
 logger = logging.getLogger(__name__)
 
 
-def ip_address_from_network_interface(interface: str) -> str:
+def ip_address_and_netmask_from_network_interface(
+    interface: str,
+) -> tuple[str, str, str]:
     """Get the ip address either the inet4 or 6 from the network interface provided.
 
     Args:
@@ -15,28 +16,35 @@ def ip_address_from_network_interface(interface: str) -> str:
 
     Returns:
         A string format of the ip address as *.*.*.*
+        The netmask as a str
+        Then the *.*.*.*/*
 
     Raises:
         OSError
             when the interface does not exist
 
     Examples:
-        >>> distrax.utils.network.ip_address_from_network_interface("lo")
-            '127.0.0.1'
+        >>> distrax.utils.network.ip_address_and_netmask_from_network_interface("lo")
+            '127.0.0.1', '32', '127.0.0.1/32'
     """
     SIOCGIFADDR = 0x8915
+    SIOCGIFNETMASK = 0x891B
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     bytes_version_interface = struct.pack("256s", interface.encode("utf_8"))
     try:
         packed_addr = fcntl.ioctl(sock.fileno(), SIOCGIFADDR, bytes_version_interface)[
             20:24
         ]
+        netmask = socket.inet_ntoa(
+            fcntl.ioctl(sock.fileno(), SIOCGIFNETMASK, bytes_version_interface)[20:24]
+        )
+        netmask = str(sum(bin(int(x)).count("1") for x in netmask.split(".")))
     except OSError as error:
-        logger.error(f"Interface `{interface}` does not exists")
+        logger.debug(f"Interface `{interface}` does not exists")
         raise (error)
     sock.close()
     ip_address = socket.inet_ntop(socket.AF_INET, packed_addr)
-    return ip_address
+    return ip_address, netmask, f"{ip_address}/{netmask}"
 
 
 def hostname() -> str:
@@ -50,19 +58,3 @@ def hostname() -> str:
             'example.com'
     """
     return socket.gethostname()
-
-
-def ip_with_netmask(ip_address: str) -> str:
-    """From the ip address get the netmask.
-
-    Args:
-        ip_address: the address to get the netmask added to
-
-    Returns:
-        a string of the ip address with the netmask added
-
-    Examples:
-        >>> distrax.utils.network.ip_with_netmask("127.0.0.1")
-            '127.0.0.1/32'
-    """
-    return ipaddress.ip_interface(ip_address).exploded
